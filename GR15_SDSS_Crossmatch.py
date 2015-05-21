0360bdaf9d8ca9312f830b2576bd803be8a0e510
Austen Groener <amg338@drexel.edu>
2015-05-21 12:38:07 -0400

    Added a few bookkeeping functions for data retrieval from the excel file (Groener et al. 2015), along with functions to convert RA/Dec to decimal format. Also, created a function called clusters_within_region to find (and plot) clusters between RA/Dec bounds.

---
+++ GR15_SDSS_Crossmatch.py
@@ -0,0 +1,110 @@
+from xlrd import open_workbook
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Temporary imports
+import ipdb
+
+# /--- Functions ---/ #
+def ra_to_deg(ra):
+    pieces = ra.split(' ')
+    hrs = pieces[0]
+    mins = pieces[1]
+    secs = pieces[2]
+    return float(hrs)*(360/24) + float(mins)*(15./60.) + float(secs)*(15./3600.)
+
+def dec_to_deg(dec):
+    pieces = dec.split(' ')
+    #check to see if unicode minus sign is present
+    if dec[0] == u'\u2212':
+        degs = -1*float(pieces[0][1:])
+        mins = -1*float(pieces[1])
+        secs = -1*float(pieces[2])
+        return degs+mins/60.+secs/3600.
+    # if not, proceed normally
+    else:
+        degs = float(pieces[0])
+        mins = float(pieces[1])
+        secs = float(pieces[2])
+        return degs+mins/60.+secs/3600.
+
+def startup():
+    '''
+    Retrieves all data from Groener et al. 2015 study.
+    '''
+    # Opening Excel File
+    wb = open_workbook('/Users/groenera/Desktop/Dropbox/Private/Research/DataFiles/ObservedClusterConcsDB/cm_data.xlsx')
+    # First sheet
+    sheet_names = wb.sheet_names()
+    sheet1 = wb.sheet_by_name(sheet_names[0])
+    # Get headers and data
+    clusters = sheet1.col_values(0)[1:]
+    redshift = sheet1.col_values(1)[1:]
+    methods = sheet1.col_values(2)[1:]
+    c200 = sheet1.col_values(3)[1:]
+    c200_plus = sheet1.col_values(4)[1:]
+    c200_minus = sheet1.col_values(5)[1:]
+    m200 = sheet1.col_values(6)[1:]
+    m200_plus = sheet1.col_values(7)[1:]
+    m200_minus = sheet1.col_values(8)[1:]
+    cvir = sheet1.col_values(9)[1:]
+    cvir_plus = sheet1.col_values(10)[1:]
+    cvir_minus = sheet1.col_values(11)[1:]
+    mvir = sheet1.col_values(12)[1:]
+    mvir_plus = sheet1.col_values(13)[1:]
+    mvir_minus = sheet1.col_values(14)[1:]
+    short_refs = sheet1.col_values(15)[1:]
+    orig_convention = sheet1.col_values(16)[1:]
+    cosmology = sheet1.col_values(17)[1:]
+    ra = sheet1.col_values(-2)[1:]
+    dec = sheet1.col_values(-1)[1:]
+    return clusters,redshift,methods,c200,c200_plus,c200_minus,m200,m200_plus,m200_minus,cvir,cvir_plus,cvir_minus,mvir,mvir_plus,mvir_minus,short_refs,orig_convention,cosmology,ra,dec
+
+def clusters_within_region(ra_min,ra_max,dec_min,dec_max,plotregion=False):
+    # mandate that min values are smaller than max values
+    assert ra_min < ra_max, "Minimum R.A. must be smaller than maximum value."
+    assert dec_min < dec_max, "Minimum Dec. must be smaller than maximum value."
+    # all unique cluster names, RA/Dec values, and redshifts
+    unique_clusters = [i for i in set(clusters)]
+    unique_ra = [ra_to_deg(ra[clusters.index(i)]) for i in unique_clusters]
+    unique_dec = [dec_to_deg(dec[clusters.index(i)]) for i in unique_clusters]
+    unique_z = [redshift[clusters.index(i)] for i in unique_clusters]
+    # select within RA/Dec region
+    cl_list = [unique_clusters[i] for i in range(len(unique_clusters))
+               if unique_ra[i] >= ra_min
+               and unique_ra[i] <= ra_max
+               and unique_dec[i] >= dec_min
+               and unique_dec[i] <= dec_max]
+    ra_list = [unique_ra[i] for i in range(len(unique_clusters))
+               if unique_ra[i] >= ra_min
+               and unique_ra[i] <= ra_max
+               and unique_dec[i] >= dec_min
+               and unique_dec[i] <= dec_max]
+    dec_list = [unique_dec[i] for i in range(len(unique_clusters))
+               if unique_ra[i] >= ra_min
+               and unique_ra[i] <= ra_max
+               and unique_dec[i] >= dec_min
+               and unique_dec[i] <= dec_max]
+    z_list = [unique_z[i] for i in range(len(unique_clusters))
+               if unique_ra[i] >= ra_min
+               and unique_ra[i] <= ra_max
+               and unique_dec[i] >= dec_min
+               and unique_dec[i] <= dec_max]
+    if plotregion is True:
+        # do some plotting here
+        for i in range(len(cl_list)):
+            plt.scatter(ra_list[i],dec_list[i])
+        plt.xlim(0,360)
+        plt.ylim(-90,90)
+        plt.show()
+    return cl_list,ra_list,dec_list,z_list
+        
+
+# /--- Preliminary Stuff ---/ #
+clusters,redshift,methods,c200,c200_plus,c200_minus,m200,m200_plus,m200_minus,cvir,cvir_plus,cvir_minus,mvir,mvir_plus,mvir_minus,short_refs,orig_convention,cosmology,ra,dec = startup()        
+        
+if __name__ == "__main__":
+
+    cl_list,ra_list,dec_list,z_list = clusters_within_region(ra_min=100,ra_max=270,dec_min=-10,dec_max=70,plotregion=True)
+    ipdb.set_trace()
+    
