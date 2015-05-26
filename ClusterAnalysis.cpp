4fcadfd0704207ece1bbced4f71307972c2fcaa6
Austen Groener <rocketboyausten@gmail.com>
2015-05-25 21:36:15 -0400

    Began adding cpp files for processing the sdss dr10 galaxy data, to look for environmental properties around clusters.

---
+++ ClusterAnalysis.cpp
@@ -0,0 +1,19 @@
+/* Main program, which imports sdss dr10 galaxy data, uses this data to determine properties of the environment around clusters of interest, and finds correlations with reconstructed properties like concentration and mass with its environment. */
+
+#include <iostream>
+
+#include 'import_data.h'
+
+
+int main{
+
+  // Import sdss dr10 data from file
+
+  // Segment sdss data around clusters from GR14.1.
+
+  // Analyze the large scale structure around clusters
+
+  // Determine if concentration and environment correlate
+  
+  return 0;
+}
---
+++ import_data.h
@@ -0,0 +1,8 @@
+/* Functions to import sdss dr10 data and return to anothe program */
+
+#include <iostream>
+
+int main{
+  
+  return 0;
+}
