42d24204d11077bdabced90cf9174787fc3fdd23
Austen Groener <amg338@drexel.edu>
2015-05-26 12:27:50 -0400

    Renamed import_data.h to importdata.h. Also, began thinking about the structure of the data which I will want to have.

--- import_data.h
+++
@@ -1,8 +0,0 @@
-/* Functions to import sdss dr10 data and return to anothe program */
-
-#include <iostream>
-
-int main{
-  
-  return 0;
-}
---
+++ importdata.h
@@ -0,0 +1,30 @@
+/* Functions to import sdss dr10 data and return to anothe program */
+
+#include <iostream>
+#include <string>
+
+class GalaxyData
+{
+ public:
+  GalaxyData();
+  float* getRA();
+  float* getDEC();
+  getZ();
+  
+ private:  
+  
+}
+
+void GalaxyData::GalaxyData (char* path)
+{
+  
+}
+
+
+
+
+
+int main{
+  
+  return 0;
+}
